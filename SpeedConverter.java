f4bc3a49afdd4b56ed67884f88b8d185c1bcff99
ketanram-am <ketanramamara123@gmail.com>
2025-09-13 13:46:42 +0530

    Add files via upload

---
+++ SpeedConverter.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class SpeedConverter {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Eneter kilometers per hour: ");
+            double km=sc.nextDouble();
+
+            printConversion(km);
+
+            sc.close();
+
+        }
+    
+    
+    public static long toMilesPerHour(double kilometersPerHour){
+        
+        if(kilometersPerHour<0){
+            return -1;
+        }
+        
+        
+        return Math.round(kilometersPerHour/1.609);
+        
+    }
+    
+    public static void printConversion(double kilometersPerHour){
+        
+    
+        
+    
+        if(toMilesPerHour(kilometersPerHour)<0)
+        {
+         System.out.println("Invalid Value");   
+        }else{
+        
+        System.out.println(kilometersPerHour +" km/h = "+toMilesPerHour(kilometersPerHour)+" mi/h");
+        } 
+    }
+}
+
+
